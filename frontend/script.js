4a43712c27286741a029a53379e47ec34cf2b879
shomy_47 <shabanishomy264@gmail.com>
2026-02-04 14:53:47 +0300

    add basic cart interaction

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,12 @@
 // JavaScript code will be added here
+document.addEventListener("DOMContentLoaded", function () {
+
+    const buttons = document.querySelectorAll(".food button");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            alert("Item added to cart!");
+        });
+    });
+
+});
