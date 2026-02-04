071465e5b3c3301848799f37e067acf47ddc8ee0
shomy_47 <shabanishomy264@gmail.com>
2026-02-04 15:29:04 +0300

    Add event listener for food button clicks

---
+++ script.js
@@ -0,0 +1,12 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const buttons = document.querySelectorAll(".food button");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            alert("Item added to cart!");
+        });
+    });
+
+});
